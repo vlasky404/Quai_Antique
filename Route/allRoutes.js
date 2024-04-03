9403abc56ea20e80c2f9320a753499c0a672e20d
vlasky404 <pierre.herbillion@outlook.com>
2024-04-03 18:35:30 +0200

    renamed:    Router/Route.js -> Route/Route.js 	renamed:    Router/allRoutes.js -> Route/allRoutes.js 	renamed:    Router/router.js -> Route/router.js 	modified:   index.html 	renamed:    pages/404.html -> page/404.html 	renamed:    pages/home.html -> page/home.html

--- Route/allRoutes.js
+++ Route/allRoutes.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/page/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
--- Route/router.js
+++ Route/router.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 import { allRoutes, websiteName } from "./allRoutes.js";
 
 // Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html");
+const route404 = new Route("404", "Page introuvable", "/page/404.html");
 
 // Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
